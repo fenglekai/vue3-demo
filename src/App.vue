e8815019fc8f405695faf97251403f35e8cb8932
冯乐铠 <674640130@qq.com>
2021-12-14 05:48:52 +0000

    new src

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
+</script>
+
+<template>
+  <Home class="animate__animated animate__fadeIn" />
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+p {
+  margin-block-start: 0em;
+  margin-block-end: 0em;
+}
+</style>
